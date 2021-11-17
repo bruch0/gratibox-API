cd84470ce24ae2f50a96342f3570383bda229430
bruch0 <lucas.bruch0@gmail.com>
2021-11-17 11:30:33 -0300

    feat: create user and return email and passowrd

--- tests/factories/userFactory.js
+++ tests/factories/userFactory.js
@@ -1,14 +1,23 @@
 import fakerbr from 'faker-br';
+import { hashSync } from 'bcrypt';
 import connection from '../../src/database/database.js';
 
 const createUser = async () => {
-  await connection.query(
-    `
-    INSERT INTO
-      users (name)
-      VALUES = ($1) RETURNING id`,
-    [fakerbr.name.findName()]
-  );
+  const password = fakerbr.internet.password(8);
+  const hash = hashSync(password, 10);
+  try {
+    const result = await connection.query(
+      `
+		  INSERT INTO
+			users (name, email, password)
+			VALUES ($1, $2, $3) RETURNING email`,
+      [fakerbr.name.findName(), fakerbr.internet.email(), hash]
+    );
+
+    return { email: result.rows[0].email, password };
+  } catch {
+    return 500;
+  }
 };
 
 export default createUser;
