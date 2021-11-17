fab03e89158bfe9f575dd4182de9ff7a99c58051
bruch0 <lucas.bruch0@gmail.com>
2021-11-17 17:07:34 -0300

    feat: creates a jwt

--- src/controllers/signIn.js
+++ src/controllers/signIn.js
@@ -1,4 +1,5 @@
 import { compareSync } from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import connection from '../database/database.js';
 import signInSchema from '../schemas/signInSchema.js';
 
@@ -9,7 +10,7 @@ const signIn = async (req, res) => {
     return res.sendStatus(400);
   }
 
-  const validation = signInSchema.validate({ email, password });
+  const validation = signInSchema.validate(req.body);
 
   if (validation.error) {
     return res.sendStatus(400);
@@ -25,8 +26,23 @@ const signIn = async (req, res) => {
       return res.sendStatus(404);
     }
 
+    const { id } = result.rows[0];
+
     if (compareSync(password, result.rows[0].password)) {
-      return res.sendStatus(200);
+      const sessionId = await connection.query(
+        'INSERT INTO sessions (user_id) VALUES ($1) RETURNING id',
+        [id]
+      );
+
+      const jwtSecret = process.env.JWT_SECRET;
+      const configurations = { expiresIn: 60 * 15 };
+      const token = jwt.sign(
+        { sessionId: sessionId.rows[0].id },
+        jwtSecret,
+        configurations
+      );
+
+      return res.status(200).send({ token });
     }
 
     return res.sendStatus(401);
