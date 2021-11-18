74bfcbe5bc6d2eae0b6b79576b86bee3a1edfd61
bruch0 <lucas.bruch0@gmail.com>
2021-11-18 12:09:44 -0300

    feat: get user subscription

--- src/app.js
+++ src/app.js
@@ -3,13 +3,15 @@ import cors from 'cors';
 
 import signUp from './controllers/signUp.js';
 import signIn from './controllers/signIn.js';
-import { subscribeUser, changeSubscription } from './controllers/subscribe.js';
 import authenticationJWT from './middlewares/authenticationJWT.js';
+import { subscribeUser, changeSubscription } from './controllers/subscribe.js';
+import getUserSubscription from './controllers/getUserSubscription.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/subscribe', authenticationJWT);
+app.use('/get-subscription', authenticationJWT);
 
 app.post('/sign-up', signUp);
 
@@ -18,5 +20,7 @@ app.post('/sign-in', signIn);
 app.post('/subscribe', subscribeUser);
 
 app.put('/subscribe', changeSubscription);
+
+app.get('/get-subscription', getUserSubscription);
 
 export default app;
---
+++ src/controllers/getUserSubscription.js
@@ -0,0 +1,35 @@
+import connection from '../database/database.js';
+
+const getUserSubscription = async (req, res) => {
+  const { sessionId } = req;
+
+  try {
+    const user = await connection.query(
+      'SELECT * FROM sessions WHERE id = $1',
+      [sessionId]
+    );
+    const userId = user.rows[0].user_id;
+
+    const result = await connection.query('SELECT * FROM users WHERE id = $1', [
+      userId,
+    ]);
+
+    const subscriptionId = result.rows[0].subscription_id;
+
+    if (!subscriptionId) {
+      return res.send({ subscriptionId });
+    }
+
+    const subscription = await connection.query(
+      'SELECT * FROM subscriptions WHERE id = $1',
+      [subscriptionId]
+    );
+    const subscriptionName = subscription.rows[0].name;
+
+    return res.send({ subscriptionName });
+  } catch {
+    return res.sendStatus(500);
+  }
+};
+
+export default getUserSubscription;
