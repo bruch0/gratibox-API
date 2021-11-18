c16c621a736678af699714f5cc421cd06315cb73
bruch0 <lucas.bruch0@gmail.com>
2021-11-18 20:36:59 -0300

    feat: get delivered boxes

--- src/app.js
+++ src/app.js
@@ -6,17 +6,19 @@ import signIn from './controllers/signIn.js';
 import persistLogin from './controllers/persistLogin.js';
 import authenticationJWT from './middlewares/authenticationJWT.js';
 import { subscribeUser, changeSubscription } from './controllers/subscribe.js';
-import getUserSubscription from './controllers/getUserSubscription.js';
-import getUserInfo from './controllers/getUserInfo.js';
+import getUserSubscription from './controllers/userSubscription.js';
+import getUserInfo from './controllers/userInfo.js';
 import registerFeedback from './controllers/feedback.js';
+import getDeliveredBoxes from './controllers/deliveredBoxes.js';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/subscribe', authenticationJWT);
-app.use('/get-subscription', authenticationJWT);
+app.use('/user-subscription', authenticationJWT);
 app.use('/user-info', authenticationJWT);
 app.use('/feedback', authenticationJWT);
+app.use('/delivered-boxes', authenticationJWT);
 
 app.post('/sign-up', signUp);
 
@@ -28,10 +30,12 @@ app.post('/subscribe', subscribeUser);
 
 app.put('/subscribe', changeSubscription);
 
-app.get('/get-subscription', getUserSubscription);
+app.get('/user-subscription', getUserSubscription);
 
 app.get('/user-info', getUserInfo);
 
 app.post('/feedback', registerFeedback);
+
+app.get('/delivered-boxes', getDeliveredBoxes);
 
 export default app;
---
+++ src/controllers/deliveredBoxes.js
@@ -0,0 +1,35 @@
+import connection from '../database/database.js';
+
+const getDeliveredBoxes = async (req, res) => {
+  const { sessionId, newToken } = req;
+
+  try {
+    const userSession = await connection.query(
+      'SELECT * FROM sessions WHERE id = $1',
+      [sessionId]
+    );
+    const userId = userSession.rows[0].user_id;
+
+    const deliverys = await connection.query(
+      `SELECT * FROM deliverys WHERE user_id = $1 AND delivered = 'yes' ORDER BY id DESC`,
+      [userId]
+    );
+    const dates = [];
+
+    deliverys.rows.forEach((box) => {
+      dates.push({
+        date: box.scheduled_date,
+        id: box.id,
+      });
+    });
+
+    return res.send({
+      dates,
+      newToken,
+    });
+  } catch {
+    return res.sendStatus(500);
+  }
+};
+
+export default getDeliveredBoxes;
