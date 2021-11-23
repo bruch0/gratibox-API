0f1cd6efb68404b72526046804e6f97749af6f39
Matheus Galdino <matheus@respondeai.com.br>
2021-11-23 12:20:36 -0300

    preparando o review

--- src/schemas/feedbackSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const feedbackSchema = joi.object({
-  feedbackId: joi.number().required(),
-  boxId: joi.number().required(),
-  comment: joi.string().min(8),
-});
-
-export default feedbackSchema;
--- src/server.js
+++
@@ -1,4 +0,0 @@
-import './setup.js';
-import app from './app.js';
-
-app.listen(process.env.PORT);
